5921623ddcdf91f5b7b25cae5b97ec63555ce8fa
William Li <weeyum@gmail.com>
2015-07-11 18:24:38 -0700

    allow both get and post for analyze

--- service.rb
+++ service.rb
@@ -2,17 +2,19 @@ require 'json'
 require 'sinatra'
 require 'faraday'
 
-get '/analyze' do
-  elasticsearch_url = ENV['SEARCHBOX_SSL_URL'] || ENV['ELASTICSEARCH_URL'] || 'http://localhost:9200'
+[:get, :post].each do |type|
+  send type, '/analyze' do
+    elasticsearch_url = ENV['SEARCHBOX_SSL_URL'] || ENV['ELASTICSEARCH_URL'] || 'http://localhost:9200'
 
-  connection = Faraday.new(url: "#{elasticsearch_url}/language-analyzer-service-index/_analyze")
+    connection = Faraday.new(url: "#{elasticsearch_url}/language-analyzer-service-index/_analyze")
 
-  response = connection.get do |req|
-    req.options.timeout = 5
-    req.body = params['text']
+    response = connection.get do |req|
+      req.options.timeout = 5
+      req.body = params['text']
+    end
+
+    content_type :json
+    status response.status
+    response.body
   end
-
-  content_type :json
-  status response.status
-  response.body
 end
--- spec/test.rb
+++ spec/test.rb
@@ -14,15 +14,19 @@ end
 describe "Service" do
   let(:text) { 'hello' }
 
-  subject {  get "/analyze?text=#{text}" }
+  [:get, :post].each do |type|
+    describe "#{type}" do
+      subject { send type, "/analyze?text=#{text}" }
 
-  it "should return json" do
-    subject
-    last_response.headers['Content-Type'].must_equal 'application/json'
-  end
+      it "should return json" do
+        subject
+        last_response.headers['Content-Type'].must_equal 'application/json'
+      end
 
-  it "should return the correct tokens" do
-    subject
-    last_response.body.must_match text
+      it "should return the correct tokens" do
+        subject
+        last_response.body.must_match text
+      end
+    end
   end
 end
